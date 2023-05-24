8a251140815a2ca239c3d72e281c03878048bc75
michudud <pzlc1236@gmail.com>
2023-05-25 00:31:59 +0200

    rotate solar panel to specific angle

--- src/components/SolarFarm/SolarFarm.jsx
+++ src/components/SolarFarm/SolarFarm.jsx
@@ -1,15 +1,20 @@
-import * as THREE from "three";
 import solarPanelModel from "./models/solar_panel.glb";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const SolarFarm = () => {
   const solarPanel = useLoader(GLTFLoader, solarPanelModel);
-  let target = new THREE.Vector3();
 
   useFrame((status) => {
-    solarPanel.scene.getObjectByName("panel").rotation.x =
-      -status.scene.getObjectByName("sun_orbit").rotation.z + Math.PI / 2;
+    const sunRotation = status.scene.getObjectByName("sun_orbit").rotation.z;
+    if (Math.floor((sunRotation / Math.PI) % 2) === 0) {
+      let currAngle = ((sunRotation % Math.PI) / Math.PI) * 180;
+
+      if (currAngle > 40 && currAngle < 140) {
+        solarPanel.scene.getObjectByName("panel").rotation.x =
+          -sunRotation + Math.PI / 2;
+      }
+    }
   });
 
   return (
