33e26ec24490e597027e4346dc23ac38a827218e
michudud <pzlc1236@gmail.com>
2023-05-26 23:35:18 +0200

    add basic solar panel cloning

--- src/components/SolarFarm/SolarFarm.jsx
+++ src/components/SolarFarm/SolarFarm.jsx
@@ -6,29 +6,42 @@ const SolarFarm = () => {
   const solarPanel = useLoader(GLTFLoader, solarPanelModel);
   solarPanel.scene.getObjectByName("panel").rotation.x = Math.PI / 4;
 
-  useFrame((status) => {
-    const sunRotation = status.scene.getObjectByName("sun_orbit").rotation.z;
-    const currSunAngle = ((sunRotation % Math.PI) / Math.PI) * 180;
+  let allSolarPanels = [];
 
-    if (Math.floor((sunRotation / Math.PI) % 2) === 0) {
-      if (currSunAngle > 45 && currSunAngle < 135) {
-        solarPanel.scene.getObjectByName("panel").rotation.x =
-          -sunRotation + Math.PI / 2;
-      }
-    } else {
-      if (currSunAngle > 45 && currSunAngle < 135) {
-        solarPanel.scene.getObjectByName("panel").rotation.x =
-          sunRotation + Math.PI / 2;
-      }
-    }
-  });
+  for (let i = 0; i < 5; i++) {
+    allSolarPanels.push(solarPanel.scene.clone());
+  }
+
+  // useFrame((status) => {
+  //   const sunRotation = status.scene.getObjectByName("sun_orbit").rotation.z;
+  //   const currSunAngle = ((sunRotation % Math.PI) / Math.PI) * 180;
+
+  //   if (Math.floor((sunRotation / Math.PI) % 2) === 0) {
+  //     if (currSunAngle > 45 && currSunAngle < 135) {
+  //       solarPanel.scene.getObjectByName("panel").rotation.x =
+  //         -sunRotation + Math.PI / 2;
+  //     }
+  //   } else {
+  //     if (currSunAngle > 45 && currSunAngle < 135) {
+  //       solarPanel.scene.getObjectByName("panel").rotation.x =
+  //         sunRotation + Math.PI / 2;
+  //     }
+  //   }
+  // });
 
   return (
-    <primitive
-      object={solarPanel.scene}
-      scale={0.8}
-      rotation={[0, Math.PI / 2, 0]}
-    />
+    <group>
+      {allSolarPanels.map((panel, index) => {
+        return (
+          <primitive
+            object={panel}
+            scale={0.8}
+            rotation={[0, Math.PI / 2, 0]}
+            position={[index * 5, 0, 0]}
+          />
+        );
+      })}
+    </group>
   );
 };
 
